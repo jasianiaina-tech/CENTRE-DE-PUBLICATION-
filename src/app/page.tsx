d474fd1e04de21cf193df2f295f455a68fe79674
jasianiaina-tech <jasianiaina@gmail.com>
2026-01-29 19:40:31 +0300

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Centre de publication</h1>
+      <p>Plateforme de publication pour tous, toutes les langues.</p>
+    </main>
+  );
+}
